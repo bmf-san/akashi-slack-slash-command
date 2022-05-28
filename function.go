bf5184f9a6d338db7d24de12efe7d5265a437d57
bmf-san <bmf.infomation@gmail.com>
2022-05-28 23:08:59 +0900

    [update] minimum implement

--- function.go
+++ function.go
@@ -10,12 +10,23 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/bmf-san/akashigo/client"
+	"github.com/bmf-san/akashigo/stamp"
+	"github.com/bmf-san/akashigo/types"
 	"github.com/slack-go/slack"
 )
 
 var (
 	signingSecret string
+	apiclient     *client.Client
 )
+
+func init() {
+	// TODO: apiTokenはストレージから取得するようにするので、これは後で破棄
+	apiToken := os.Getenv("AKASHI_API_TOKEN")
+	companyID := os.Getenv("AKASHI_COMPANY_ID")
+	apiclient = client.New(apiToken, companyID)
+}
 
 func Slash(w http.ResponseWriter, r *http.Request) {
 	var signingSecret string
@@ -44,18 +55,44 @@ func Slash(w http.ResponseWriter, r *http.Request) {
 	}
 
 	switch s.Command {
-	case "/hello":
+	case "/akashi":
 		params := &slack.Msg{Text: s.Text}
-		b, err := json.Marshal(params)
+		p, err := json.Marshal(params)
 		if err != nil {
+			log.Fatal(err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
+		// TODO: paramsのバリデーションをする
+
+		model := &stamp.Stamp{
+			Client: apiclient,
+		}
+		s, err := model.Stamp(stamp.StampParams{
+			Token:    apiclient.APIToken,
+			Type:     types.StampNumber(string(p)),
+			Timezone: "+09:00",
+		})
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		b, err := json.Marshal(s)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		// TODO: 期限切れの場合はトークン再発行APIをコールして、再度リクエスト
+		//
+
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(b)
 	default:
-		log.Fatal(errors.New("No command specified"))
+		log.Fatal(errors.New("Invalid command"))
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
